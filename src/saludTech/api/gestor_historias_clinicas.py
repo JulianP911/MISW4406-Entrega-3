41583a0b2f306dcf2d1a67e83319c2582cfe4d71
Santiago Fajardo <s.fajardot@uniandes.edu.co>
2025-02-20 23:35:48 +0000

    cambio de modulo

--- src/saludTech/api/gestor_historias_clinicas.py
+++ src/saludTech/api/gestor_historias_clinicas.py
@@ -1,3 +1,3 @@
 import saludTech.seedwork.presentacion.api as api
 
-bp = api.crear_blueprint('gestor_historia_clinica', '/gestor_historia_clinica')
+bp = api.crear_blueprint('gestor_archivos', '/gestor_archivos')
